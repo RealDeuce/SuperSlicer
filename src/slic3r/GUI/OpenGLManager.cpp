4c9d40bb55dcebcfcd66418525e68f80bcc8aa8e
Stephen Hurd <deuce@synchro.net>
2022-01-05 14:12:18 -0500

    Use bundled GLEW to avoid glewInit() error
    We previously just ignored the error.

--- src/slic3r/GUI/OpenGLManager.cpp
+++ src/slic3r/GUI/OpenGLManager.cpp
@@ -235,9 +235,10 @@ OpenGLManager::~OpenGLManager()
 bool OpenGLManager::init_gl()
 {
     if (!m_gl_initialized) {
-        if (glewInit() != GLEW_OK) {
-            BOOST_LOG_TRIVIAL(error) << "Unable to init glew library";
-            //return false;
+        int ret;
+        if ((ret = glewInit()) != GLEW_OK) {
+            BOOST_LOG_TRIVIAL(error) << "Unable to init glew library: " << glewGetErrorString(ret);
+            return false;
         }
         m_gl_initialized = true;
         if (GLEW_EXT_texture_compression_s3tc)
