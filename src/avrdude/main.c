a7f1a95c1a2626d87a89dfa61c363895f7fbce78
Steve Wills <steve@mouf.net>
2025-05-28 09:39:41 -0400

    remove another uneccessary change

--- src/avrdude/main.c
+++ src/avrdude/main.c
@@ -806,7 +806,7 @@ int avrdude_main(int argc, char * argv [])
   avrdude_message(MSG_NOTICE, "\n%s: Version %s, compiled on %s at %s\n"
                     "%sCopyright (c) 2000-2005 Brian Dean, http://www.bdmicro.com/\n"
                     "%sCopyright (c) 2007-2014 Joerg Wunsch\n\n",
-                    progname, version, "REDACTED", "REDACTED", progbuf, progbuf);
+                    progname, version, __DATE__, __TIME__, progbuf, progbuf);
   // avrdude_message(MSG_NOTICE, "%sSystem wide configuration file is \"%s\"\n",
   //           progbuf, sys_config);
 
